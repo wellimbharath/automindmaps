2d9d5d0132ed8d784ed45267e6adb129dcc8d07f
bharathsh36 <bharathsh36@gmail.com>
2017-02-15 23:04:14 +0530

    Add Signout link

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users,  controllers: { omniauth_callbacks: "users/omniauth_callbacks", session: 'users/sessions', registration: 'users/registrations' }
 
+  devise_scope :user do
+    get 'sign_up', to: 'devise/registrations#new', as:  :sign_up
+    get 'sign_in', to: 'devise/sessions#new', as:  :sign_in
+    get 'logout', to: 'devise/sessions#destroy', as:  :logout , method: :delete
+  end
   resources :tree_views
   root to: 'home#index'
   resources :main
